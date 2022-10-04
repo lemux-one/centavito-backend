08235ae42be2e81de149879123d0124d29e5bdbb
LemuxOne <lemux.one@gmail.com>
2022-10-04 15:27:28 -0400

    add spring boot project base structure

---
+++ src/main/java/one/lemux/centavito_backend/CentavitoBackendApplication.java
@@ -0,0 +1,13 @@
+package one.lemux.centavito_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentavitoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentavitoBackendApplication.class, args);
+	}
+
+}
